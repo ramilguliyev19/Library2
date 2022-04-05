09d2f3049907fde44072c7a59dba3e8a546f9f2a
ramilguliev19 <97315721+ramilguliev19@users.noreply.github.com>
2022-04-05 14:20:34 +0400

    Add files via upload

---
+++ LibraryAgain/LibraryAgain/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAgain.Models
+{
+    public class Book<T>
+    {
+        public string AuthorName { get; set; }
+        public string Name { get; set; }
+        public int PageCount { get; set; }
+        public int Id { get; private set; }
+        public string Code { get; private set; }
+        
+        
+       
+        
+        private Book()
+
+
+        {
+           
+            Code = Name[0].ToString().ToUpper() + Id;
+        }
+
+        public Book(string name, string authorName, int pageCount) : this()
+        {
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+        }
+    }
+}
+
