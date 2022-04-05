bd56ae491737e583d5170981011e73ecd7dd6f1a
ramilguliev19 <97315721+ramilguliev19@users.noreply.github.com>
2022-04-05 14:09:00 +0400

    Add files via upload

--- LibraryAgain/LibraryAgain/Models/Book.cs
+++ LibraryAgain/LibraryAgain/Models/Book.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,3 +35,80 @@ namespace LibraryAgain.Models
     }
 }
 
+=======
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAgain.Models
+{
+    public class Book<T>
+    {
+        public string AuthorName { get; set; }
+        public string Name { get; set; }
+        public int PageCount { get; set; }
+        public int Id { get; private set; }
+        public string Code { get; private set; }
+        
+        
+       
+        
+        private Book()
+
+
+        {
+           
+            Code = Name[0].ToString().ToUpper() + Id;
+        }
+
+        public Book(string name, string authorName, int pageCount) : this()
+        {
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+        }
+    }
+}
+
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAgain.Models
+{
+    public class Book<T>
+    {
+        public string AuthorName { get; set; }
+        public string Name { get; set; }
+        public int PageCount { get; set; }
+        public int Id { get; private set; }
+        public string Code { get; private set; }
+        
+        
+       
+        
+        private Book()
+
+
+        {
+           
+            Code = Name[0].ToString().ToUpper() + Id;
+        }
+
+        public Book(string name, string authorName, int pageCount) : this()
+        {
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+        }
+    }
+}
+
+>>>>>>> d938e3c (homework)
+>>>>>>> 537eb63 (homework)
--- LibraryAgain/LibraryAgain/Models/Library.cs
+++ LibraryAgain/LibraryAgain/Models/Library.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
@@ -76,4 +77,165 @@ namespace LibraryAgain.Models
        
 
     }
+=======
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryAgain.Models
+{
+    class Library<T>
+    {
+        private T[]? _booklist = new T[0];
+
+        public T this[int index]
+        {
+            get { return _booklist[index]; }
+            set { _booklist[index] = value; }
+        }
+
+
+        public Library(int length)
+        {
+            Array.Resize(ref _booklist, length);
+        }
+
+       
+        //public List<Book> FindAllBooksByName(string name)
+        //{
+        //    Book book = Book.FindByName(name);
+        //}
+
+        public void Add(T value)
+        {
+            Array.Resize(ref _booklist, _booklist.Length + 1);
+            _booklist[^1] = value;
+        }
+
+        public void AddRange(T[] collection)
+        {
+            foreach (T item in collection)
+            {
+                Array.Resize(ref _booklist, _booklist.Length + 1);
+                _booklist[^1] = item;
+            }
+        }
+        
+       
+        public void BinarySearch(T item)
+        {
+            Array.BinarySearch(_booklist, item);
+        }
+        public int Capacity()
+        {
+            return _booklist.Length;
+        }
+      
+
+
+        public T[] FindAll()
+        {       
+           return new T[_booklist.Length];
+        }
+       
+      
+      
+       
+        public void Remove(T item)
+        {
+            _booklist[Array.IndexOf(_booklist, item)] = default(T);
+        }
+        public void RemoveAll(int down, int up)
+        {
+            Array.Clear(_booklist, down, up);
+        }
+        public void RemoveAt(int index)
+        {
+            _booklist[index] = default(T);
+        }
+       
+
+    }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryAgain.Models
+{
+    class Library<T>
+    {
+        private T[]? _booklist = new T[0];
+
+        public T this[int index]
+        {
+            get { return _booklist[index]; }
+            set { _booklist[index] = value; }
+        }
+
+
+        public Library(int length)
+        {
+            Array.Resize(ref _booklist, length);
+        }
+
+       
+        //public List<Book> FindAllBooksByName(string name)
+        //{
+        //    Book book = Book.FindByName(name);
+        //}
+
+        public void Add(T value)
+        {
+            Array.Resize(ref _booklist, _booklist.Length + 1);
+            _booklist[^1] = value;
+        }
+
+        public void AddRange(T[] collection)
+        {
+            foreach (T item in collection)
+            {
+                Array.Resize(ref _booklist, _booklist.Length + 1);
+                _booklist[^1] = item;
+            }
+        }
+        
+       
+        public void BinarySearch(T item)
+        {
+            Array.BinarySearch(_booklist, item);
+        }
+        public int Capacity()
+        {
+            return _booklist.Length;
+        }
+      
+
+
+        public T[] FindAll()
+        {       
+           return new T[_booklist.Length];
+        }
+       
+      
+      
+       
+        public void Remove(T item)
+        {
+            _booklist[Array.IndexOf(_booklist, item)] = default(T);
+        }
+        public void RemoveAll(int down, int up)
+        {
+            Array.Clear(_booklist, down, up);
+        }
+        public void RemoveAt(int index)
+        {
+            _booklist[index] = default(T);
+        }
+       
+
+    }
+>>>>>>> d938e3c (homework)
+>>>>>>> 537eb63 (homework)
 }
